a59c43038fcabdf99d2a21891e8f8e1d468880dd
guangLess <bha.guang@gmail.com>
2015-09-28 20:16:32 -0400

    base

--- DeployOnDay1Redux/FISTicTacToeGame.m
+++ DeployOnDay1Redux/FISTicTacToeGame.m
@@ -23,7 +23,7 @@
     if(self) {
         // Do initialization of your game here, inside this if statement.
         // Leave the rest of this method alone :)
-
+        [self resetBoard];
     }
 
     return self;
@@ -31,37 +31,119 @@
 
 -(void)resetBoard
 {
+    [self.board removeAllObjects];
+    self.board = [[NSMutableArray alloc] init];
+    
+    self.board = [ @[ [@[@"", @"", @""] mutableCopy],
+                      [@[@"", @"", @""] mutableCopy],
+                      [@[@"", @"", @""] mutableCopy]] mutableCopy];
+    
+    NSLog(@"%@",self.board);
 
 }
 
 -(NSString *)playerAtColumn:(NSUInteger)column row:(NSUInteger)row
 {
-    return @"";
+    NSString * player = self.board[column][row];
+    return player;
 }
 
 -(BOOL)canPlayAtColumn:(NSUInteger)column row:(NSUInteger)row
 {
-    return YES;
+    if ([self.board[column][row] isEqualToString:@""]) {
+        return YES;
+    } else {
+        return NO;
+    }
 }
 
 -(void)playXAtColumn:(NSUInteger)column row:(NSUInteger)row
 {
-
+    if ([self canPlayAtColumn:column row:row]) {
+        
+        self.board[column][row] = @"x";
+    }
 }
 
 -(void)playOAtColumn:(NSUInteger)column row:(NSUInteger)row
 {
 
+    if ([self canPlayAtColumn:column row:row]) {
+        
+        self.board[column][row] = @"O";
+    }
 }
 
 -(NSString *)winningPlayer
-{
-    return @"";
-}
+
+    {
+    
+   
+    for (NSUInteger i = 0 ; i < 3 ; i++) { // check row ->
+
+    if ([ [self playerAtColumn:0 row:i] isEqualToString:[self playerAtColumn:1 row:i]]  &&
+        [ [self playerAtColumn:2 row:i] isEqualToString:[self playerAtColumn:1 row:i]] &&
+        (![[self playerAtColumn:0 row:i] isEqualToString:@""])
+        ) {
+
+        NSString * sameValue = [self playerAtColumn:0 row:i];
+        NSLog(@"here");
+        return sameValue;
+        }
+
+    }
+    
+    for (NSUInteger i = 0 ; i < 3 ; i++) { // check colum ^
+            
+            if ([ [self playerAtColumn:i row:0] isEqualToString:[self playerAtColumn:i row:1]] &&
+                [ [self playerAtColumn:i row:2] isEqualToString:[self playerAtColumn:i row:1]] &&
+               (![[self playerAtColumn:i row:0] isEqualToString:@""])
+                ) {
+                
+                NSString * sameValue = [self playerAtColumn:i row:0];
+                NSLog(@"here2");
+                return sameValue;
+            }
+        }
+    
+        // check cross x maybe look for simpler number pattern.
+            
+        if ([ [self playerAtColumn:0 row:0 ] isEqualToString:[self playerAtColumn:(0+1) row:(0+1)]] &&
+                [ [self playerAtColumn:0 row:0] isEqualToString:[self playerAtColumn:(0+2) row:(0+2)] ] &&
+                (![[self playerAtColumn:0 row:0] isEqualToString:@""])
+                ) {
+                
+                NSString * sameValue = [self playerAtColumn:0 row:0];
+            NSLog(@"here3");
+                return sameValue;
+            }
+        
+        if ([ [self playerAtColumn:0 row:2 ] isEqualToString:[self playerAtColumn:(0+1) row:(0+1)]] &&
+            [ [self playerAtColumn:0 row:2] isEqualToString:[self playerAtColumn:2 row:0] ] &&
+            (![[self playerAtColumn:0 row:2] isEqualToString:@""])
+            ) {
+            
+            NSString * sameValue = [self playerAtColumn:0 row:0];
+            NSLog(@"here4");
+            return sameValue;
+        }
+        
+        return @"";
+        NSLog(@"here5");
+
+    }
+
 
 -(BOOL)isADraw
 {
-    return NO;
+//    if ([[self winningPlayer] isEqualToString:@"h"]){
+//        return NO;
+//        NSLog(@"NOTDRAWED");
+//   }
+//    //else return YES;
+//    NSLog(@"DRAWED");
+
+    //return NO;
 }
 
 @end
